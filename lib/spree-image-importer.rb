9362569af6b509854726b4bb7f0ac7773351166b
Rory Gianni <giannichan@gmail.com>
2013-09-30 22:06:32 +0100

    deleted guff

--- lib/spree-image-importer.rb
+++
@@ -1,2 +0,0 @@
-module SpreeImageImporter
-end
--- test/spree-image-importer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpreeImageImporterTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SpreeImageImporter
-  end
-end
