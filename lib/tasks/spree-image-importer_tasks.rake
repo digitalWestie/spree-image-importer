ad500ab54c55907bbc1f7624eb67a4bfe9b548a1
Rory Gianni <giannichan@gmail.com>
2013-09-30 22:02:58 +0100

    it begins

---
+++ lib/tasks/spree-image-importer_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :spree-image-importer do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'de22d501490460a719f3c38ab7f26df2ef33dc061e276d411f3158a954f90ca4c7c9f2bd7d8e5562919fc158286bf53de15b5af5d5c1bafb9980b44a45ee850d'
